e02b33977caa65f214ebd74f7dfbb7eb9869f8d1
Tirette-hub <nathan.amorison@gmail.com>
2023-04-05 22:08:31 +0200

    correcting 'for loop' test implementation with working logic

--- tests/premier_test_for_loop.py
+++ tests/premier_test_for_loop.py
@@ -46,12 +46,44 @@ class Variable:
     def __repr__(self):
         return f"{{{self._name} := {self._value} ({self._type})}}"
 
+class ListVariable(Variable):
+    EOL = "EOL" #End Of List
+    def __init__(self, *args):
+        super(ListVariable, self).__init__(*args)
+        self.index = 0
+
+    def get_index(self, index = None):
+        if index:
+            if (index < len(self._value)):
+                return self._value[index]
+            else:
+                raise IndexError("list index out of range")
+        
+        if (self.index < len(self._value)):
+            return self._value[self.index]
+        else:
+            raise IndexError("list index out of range")
+
+    def next(self):
+        try:
+            result = self.get_index(index = self.index+1)
+        except IndexError:
+            result = ListVariable.EOL
+        return result
+
+    def increment(self):
+        self.index+=1
+
+    def __repr__(self):
+        return f"{{{self._name} := {self._value[self.index]}, list size = {len(self._value)}, list content = {self._value}, current index = {self.index}}}"
+
 
 class SymbolTable:
     """
     Structure de données représentant une table de symbole
     """
-    def __init__(self):
+    def __init__(self, parent=None):
+        self.parent = parent
         self._table = {}
         self._next = []
 
@@ -71,10 +103,20 @@ class SymbolTable:
         """
         Récupération d'un élément dans la table
         """
-        return self._table[k]
+        if k in self._table.keys():
+            return self._table[k]
+        elif self.parent:
+            return parent.get(k)
+        else:
+            raise NameError(f"'{k}' not in symbol table")
 
     def __contains__(self, o):
-        return o in self._table
+        if o in self._table.keys():
+            return True
+        elif self.parent:
+            return o in self.parent
+        else:
+            return False
 
     def __str__(self):
         _ = "[\n"
@@ -96,6 +138,8 @@ class SymbolTable:
 class IntermediateCodeHandler:
     def __init__(self):
         self.index = 0
+        self.memory = SymbolTable()
+        self.current_scope = self.memory
         self.stack = []
         self._output_buffer = ""
 
@@ -104,50 +148,124 @@ class IntermediateCodeHandler:
         self.index +=1
         return self.index
 
-    def execute(self):
-        return self.stack
+    def execute(self, DEBUG=False):
+        if DEBUG:
+            print("DEBUG MODE IS ON")
+
+        self.index = 0
+
+        while self.index < len(self.stack):
+            task = self.stack[self.index]
+            
+            if DEBUG:
+                print("DEBUG:", task)
+
+            if task.get_type() == AExpression.PRINT:
+                if DEBUG:
+                    print("DEBUG: PRINT STATEMENT")
+                #afficher du contenu
+                to_print = task.get_content()
+                if to_print.get_type() == Variable.FOR_LIST:
+                    #gerer differemment les variables for_list
+                    self._output_buffer += str(to_print.get_index()) + "\n"
+                else:
+                    self._output_buffer += str(to_print.get()) + "\n"
+
+                self.index += 1
+
+            elif task.get_type() == AExpression.VAR:
+                if DEBUG:
+                    print("DEBUG: VARIABLE ASSIGNMENT")
+                variable = task.get_content()
+                #ajout d'une variable dans la mémoire si elle n'y est pas encore
+                if not variable.get_name() in self.current_scope:
+                    self.current_scope.add_content(variable)
+
+                self.index += 1
+
+            elif task.get_type() == AExpression.FOR:
+                if DEBUG:
+                    print("DEBUG: BEGINNING FOR LOOP")
+                #gérer un nouveau scope
+                new_scope = SymbolTable(parent = self.current_scope)
+                self.current_scope.add_depth(new_scope)
+                self.current_scope = new_scope
+                #ajouter la loop variable dans ce scope ou check s'il n'existe pas déjà une variable de ce nom
+                loop_var = task.get_content()
+                if not loop_var.get_name() in self.current_scope:
+                    new_scope.add_content(loop_var)
+
+                self.index += 1
+
+            elif task.get_type() == AExpression.ENDFOR:
+                if DEBUG:
+                    print("DEBUG: ENDING FOR LOOP OR JUMP")
+                #deal with scopes
+                index, loop_var = task.get_content()
+                loop_var = self.current_scope.get(loop_var.get_name())
+                if loop_var.next() != ListVariable.EOL:
+                    #On n'a pas encore parcouru toute la liste donc on retourne au début de la boucle
+                    loop_var.increment()
+                    self.index = index
+                else:
+                    #on regarde les instructions suivantes
+                    self.index += 1
+
+        return self._output_buffer
+
+
 
 class AExpression:
     PRINT = "PRINT"
     VAR = "VAR"
     FOR = "FOR"
+    ENDFOR = "ENDFOR"
     JUMP = "JUMP"
+
+    def __init__(self, type, content):
+        self._type = type
+        self.content = content
+
+    def get_content(self):
+        return self.content
 
     def get_type(self):
         return self._type
 
-    def execute(self):
-        pass
+    def __repr__(self):
+        return f"{self._type}: {self.content}"
 
 class Printing(AExpression):
     def __init__(self, content):
-        self._type = AExpression.PRINT
-        self.content = content
+        super(Printing, self).__init__(AExpression.PRINT, content) #on stocke une variable
 
 class VariableAssignment(AExpression):
     def __init__(self, content):
-        self._type = AExpression.VAR
-        self.variable = content
+        super(VariableAssignment, self).__init__(AExpression.VAR, content) #on stocke une variable
 
 class ForLoop(AExpression):
     def __init__(self, content):
-        self._type = AExpression.FOR
-        self.var, self.expressions = content
+        super(ForLoop, self).__init__(AExpression.FOR, content) #on stocke une variable
+
+class EndFor(AExpression):
+    def __init__(self, content):
+        super(EndFor, self).__init__(AExpression.ENDFOR, content) #index et le nom d'une variable (tuple)
 
 class Jump(AExpression):
     def __init__(self, content):
-        self._type = AExpression.JUMP
-        self.content = content
+        super(Jump, self).__init__(AExpression.JUMP, content) #index
 
 
 # Création d'un transformateur, ébauche du projet
 class ArithmeticTransformer(Transformer):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, DEBUG = False, *args, **kwargs):
         super(ArithmeticTransformer, self).__init__(*args, **kwargs)
-        self.counter = 0
         self._output_buffer = ""
         self.symbolTable = SymbolTable()
         self.inter = IntermediateCodeHandler()
+
+        self.DEBUG = DEBUG
+        self.counter = 0
 
     def expression_list(self, items):
         return items
@@ -156,8 +274,10 @@ class ArithmeticTransformer(Transformer):
         return items
 
     def arithmetic_expression(self, items):
-        print("arithmetic_expression", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("arithmetic_expression", self.counter)
+            self.counter+=1
+
         if items[0] == "(":
             return items[1]
 
@@ -177,64 +297,84 @@ class ArithmeticTransformer(Transformer):
         return result
 
     def string_list_interior(self,items):
-        print("string_list_interior", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("string_list_interior", self.counter)
+            self.counter+=1
+
         result = [items[0]]
         if len(items) > 1:
             result += items[1]
         return result
 
     def string_list(self, items):
-        print("string_list", self.counter)
-        self.counter+=1
-        var = Variable("__ANON__", Variable.LIST, items[0])
+        if self.DEBUG:
+            print("string_list", self.counter)
+            self.counter+=1
+
+        var = ListVariable("__ANON__", Variable.LIST, items[0])
         return var
 
     def string(self, items):
-        print("string", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("string", self.counter)
+            self.counter+=1
+
         result = items[0].replace("'","")
         var = Variable("__ANON__", Variable.STRING, result)
         return var
 
     def dumbo_bloc(self, items):
-        print("dumbo_bloc", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("dumbo_bloc", self.counter)
+            self.counter+=1
+
         #create new scope
         return items
 
     def print_expression(self, items):
-        print("print_expression", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("print_expression", self.counter)
+            self.counter+=1
+
         #self._output_buffer += str(items[0].get()) + "\n"
         result = f"print({str(items[0].get())})"
-        self.inter.add_instr(Printing(str(items[0].get())))
+        self.inter.add_instr(Printing(items[0]))
         return result
 
     def for_loop_expression(self, items):
-        print("for_loop_expression", self.counter)
-        self.counter+=1
-        loop_var, expression_list = items
+        if self.DEBUG:
+            print("for_loop_expression", self.counter)
+            self.counter+=1
 
-        self.inter.add_instr(ForLoop(expression_list))
+        index_and_var_name, expression_list = items
 
-        return "None"
+        self.inter.add_instr(EndFor(index_and_var_name))
+
+        return None #pas besoin de renvoyer quoi que ce soit, l'expression est terminée
 
     def loop_variable_assignment(self, items):
-        print("loop_variable_assignment", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("loop_variable_assignment", self.counter)
+            self.counter+=1
+
         loop_var, iterable = items
 
-        loop_var._type = Variable.FOR_LIST
-        loop_var._value = iterable.get()
+        #Check iterable est bien iterable
+        if not iterable.get_type() == Variable.LIST:
+            raise TypeError(f"{iterable.get_name()} ({iterable.get_type()}) not iterable")
+
+        loop_var = ListVariable(loop_var.get_name(), Variable.FOR_LIST, iterable.get())
 
         self.symbolTable.add_content(loop_var)
+        index = self.inter.add_instr(ForLoop(loop_var))
 
-        return "items"
+        return (index, loop_var)
 
     def string_expression(self, items):
-        print("string_expression", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("string_expression", self.counter)
+            self.counter+=1
+
         if len(items) > 1:
             new_items = []
             for item in items:
@@ -248,15 +388,19 @@ class ArithmeticTransformer(Transformer):
         return items[0]
 
     def variable(self, items):
-        print("variable", items[0], self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("variable", items[0], self.counter)
+            self.counter+=1
+
         if items[0] in self.symbolTable:
             return self.symbolTable.get(items[0])
         return Variable(items[0], None, None)
 
     def assignment_expression(self, items):
-        print("assignment_expression", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("assignment_expression", self.counter)
+            self.counter+=1
+
         var = items[2]
         if not var.get_type():
             raise NameError(f"name '{item.get_name()}' is not defined")
@@ -264,11 +408,13 @@ class ArithmeticTransformer(Transformer):
         #add var in scope
         self.symbolTable.add_content(var)
         self.inter.add_instr(VariableAssignment(var))
-        return "variable"
+        return None #pas besoin de retourner quoi que ce soit, l'expression est finie
 
     def signed_decimal_integer(self, items):
-        print("signed_decimal_integer", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("signed_decimal_integer", self.counter)
+            self.counter+=1
+
         if (items[0] == "-"):
             items[1]._value = -items[1].get()
             return items[1]
@@ -276,30 +422,35 @@ class ArithmeticTransformer(Transformer):
         return items[1]
 
     def decimal_integer(self, items):
-        print("decimal_integer", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("decimal_integer", self.counter)
+            self.counter+=1
+
         if len(items) == 0:
-            # value = 0
             return Variable("__ANON__", Variable.INT, 0)
         return Variable("__ANON__", Variable.INT, int("".join(items)))
 
     def non_zero_digit(self, items):
-        print("non_zero_digit", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("non_zero_digit", self.counter)
+            self.counter+=1
+
         return items[0].value
 
     def digit(self, items):
-        print("digit", self.counter)
-        self.counter+=1
+        if self.DEBUG:
+            print("digit", self.counter)
+            self.counter+=1
+
         if len(items) == 0:
             return "0"
         return items[0]
 
     def get(self):
-        return self._output_buffer
+        return self.inter.execute(DEBUG = self.DEBUG)
 
 #ouverture du fichier contenant la grammaire et création du parser à partir de cette grammaire
-arithmetic_parser = Lark.open("my_grammar.lark", parser='lalr', rel_to=__file__)
+arithmetic_parser = Lark.open("../dumbo/dumbo.lark", parser='lalr', rel_to=__file__)
 
 #ligne à tester avec la grammaire
 input_to_parse = """{{
@@ -308,12 +459,22 @@ input_to_parse = """{{
     print i;
     endfor;
     }}"""
-#"{{ print \'Hello World\'; }}" #works
-#"{{ 2 + 5 * 3; }}" #doesn't work: on ne peut pas faire simplement un calcul, il manque quelque chose devant (print, assignation de variable, ...)
+"""
+{{
+    a := 'test';
+    print a;
+}}
+"""
 
 #parsing de la ligne de test et affichage du résultat
 parsing = arithmetic_parser.parse(input_to_parse)
-print(parsing.pretty()) #affiche l'arbre du programme donné dans "input_to_parse"
-parsed = ArithmeticTransformer()
+#print(parsing.pretty()) #affiche l'arbre du programme donné dans "input_to_parse"
+parsed = ArithmeticTransformer() # ArithmeticTransformer(DEBUG = True)
 parsed.transform(parsing)
-print("parsed", parsed.get()) #affiche l'output du parsing
+
+print("######## parsed ########\n")
+
+print("\n######## OUTPUT ########\n")
+
+print(parsed.get())
+#print("parsed", parsed.get()) #affiche l'output du parsing
